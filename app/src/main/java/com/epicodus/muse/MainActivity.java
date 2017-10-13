29bea0732498674b7e2b0351f7f1c18bd6cfa20c
jessicakenney <jessicakenney@yahoo.com>
2017-10-13 16:39:51 -0700

    startup files

---
+++ app/src/main/java/com/epicodus/muse/MainActivity.java
@@ -0,0 +1,32 @@
+package com.epicodus.muse;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams.ttf");
+        mAppNameTextView.setTypeface(typeface);
+
+    }//onCreate
+
+    @Override
+    public void onClick(View v) {
+
+    }//onClick
+
+
+
+}//Main
