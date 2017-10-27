58db16f07671c2309177073d9fbb9ff9a7c492e9
Guest User <Guest@Epicodus-3B.local>
2017-10-26 18:19:11 -0700

    added user login and registration

--- app/src/main/java/com/epicodus/muse/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/muse/ui/MainActivity.java
@@ -3,13 +3,19 @@ package com.epicodus.muse.ui;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.epicodus.muse.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.hewittTextView) TextView mHewittTextView;
     @Bind(R.id.colorsButton) Button mColorsButton;
     @Bind(R.id.randomButton) Button mRandomButton;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +44,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mCooperTextView.setOnClickListener(this);
         mHewittTextView.setOnClickListener(this);
 
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //display welcome message
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    getSupportActionBar().setTitle("Welcome, " + user.getDisplayName());
+                } else {
+
+                }
+            }
+        };
+
     }//onCreate
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_login, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        MenuItem menuLogin = menu.findItem(R.id.action_login);
+        MenuItem menuLogout = menu.findItem(R.id.action_logout);
+        MenuItem menuGalleries = menu.findItem(R.id.action_galleries);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        Log.d(TAG, ">>>: mCurrentUser >>>> "+user);
+
+        //if (mAuthListener != null) {
+        if(user != null ) {
+            menuLogin.setVisible(false);
+            menuLogout.setVisible(true);
+            menuGalleries.setVisible(true);
+        } else {
+            menuLogin.setVisible(true);
+            menuLogout.setVisible(false);
+            menuGalleries.setVisible(false);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_login) {
+            login();
+            return true;
+        }
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public void onClick(View v) {
@@ -57,6 +138,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         };
 
     }//onClick
+
+    private void login() {
+        //go to loginActivity
+        Log.v(TAG, ".>>>>>...going to Login Activity");
+        Intent intent = new Intent (MainActivity.this,LoginActivity.class );
+        startActivity(intent);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
 
 
 
